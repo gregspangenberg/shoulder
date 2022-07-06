5ce121608a81157bf046a81b60f432749dd23386
gregspangenberg <gspangen@uwo.ca>
2022-07-06 14:59:39 -0400

    rework skip_i

--- src/head_articular.py
+++ src/head_articular.py
@@ -1,9 +1,11 @@
+import utils
 import matplotlib.pyplot as plt
 import circle_fit
 import pandas as pd
 import numpy as np
 import shapely.geometry
 import shapely.ops
+import skspatial.objects
 from scipy.spatial import KDTree
 from itertools import islice, cycle
 
@@ -16,15 +18,16 @@ def rolling_cirle_fit(pts, seed_pt):
     r_pts = np.roll(pts, shift=-i, axis=0) # shift (with wrap around) array until articular point is at beginning
     iters = cycle((iter(r_pts), reversed(r_pts)))
     ra_pts = np.vstack([next(it) for it in islice(iters, len(r_pts))]) # rolled and alterating both directions
-
+    
     residuals = []
     dt_residuals = []
-    skip_threshold = 0.3
+    skip_threshold = 0.4
     skip_i = None
     fit_pts = []
     for i,pt in enumerate(ra_pts):
 
         if len(fit_pts)<3:
+
             fit_pts.append(pt)
             continue
         
@@ -43,23 +46,29 @@ def rolling_cirle_fit(pts, seed_pt):
                     fit_pts.append(pt)
                 else:
                     continue
+
             xc,yc, radius, residual = circle_fit.least_squares_circle(np.vstack(fit_pts))
             residuals.append(residual)
 
-            if len(fit_pts)==3:
+            if len(fit_pts)<5:
                 dt_residuals.append(0)
             else:
-                dt_residuals.append(residual-residuals[-1])
+                dt_residuals.append(residuals[-1]-residuals[-2])
             
+
             if dt_residuals[-1] > skip_threshold:
-                if skip_i != None:
-                    skip_i = i
+                if skip_i == None:
+                    print('\n1st THRESHOLD')
+                    del fit_pts[-1] # remove point that exceeded threshold
+                    skip_i = len(fit_pts)-1
                 else:
-                    skip_i = [skip_i,i] # location in array of final stop points for each direction
+                    print('\n 2ndTHRESHOLD')
+                    del fit_pts[-1] # remove point that exceeded threshold
+                    skip_i = [skip_i,len(fit_pts)] # location in array of final stop points for each direction
                     break
 
 
-def plane(mesh, transform, articular_pt):
+def plane(mesh, transform, articular_pt, head_central_axis):
 
     # get length of the tranformed bone
     total_length = np.sum(abs(mesh.bounds[:,-1])) # entire length of bone
@@ -80,13 +89,10 @@ def plane(mesh, transform, articular_pt):
 
         # get shapely object from path
         polygon = slice.polygons_closed[0]
-        # undo translation that is applied when moving from path3d to path2d
-        translation = to_3d[:2,3:].T[0] # find x and y translation that occured
-        polygon = shapely.affinity.translate(polygon, xoff=translation[0], yoff=translation[1])
 
         polygons.append(polygon)
         zs.append(cut)
-    df = pd.DataFrame({
+    df_z = pd.DataFrame({
     'poly':polygons,
     'z':zs
     })
@@ -96,29 +102,47 @@ def plane(mesh, transform, articular_pt):
     total_width = np.sum(abs(mesh.bounds[:, 1])) # entire length of bone
     pos_width = mesh.bounds[mesh.bounds[:, 1]>=0, 1] # bone in positive space, bone before the centerline 
     neg_width = mesh.bounds[mesh.bounds[:, 1]<=0, 1] # bone in negative space, bone past the centerline 
+    #specify percentage of cutoffs
+    posterior_cutoff = 0.2*pos_width # yes posterior is in positive space for some reason
+    anterior_cutoff = 0.2*neg_width
 
-    posterior_cutoff = 0.75*total_width + neg_width # yes posterior is in positive space for some reason
-    anterior_cutoff = -0.75*total_width + pos_width
+    # move seed point(articular_point) from CT csys to new csys
+    hc_pt = utils.transform_pts(articular_pt, transform)
+
+    # direction of section
+    normal = skspatial.objects.Line.best_fit(utils.transform_pts(head_central_axis,transform)).direction
+    rotate90_z = np.array([
+        [0,1,0,0],
+        [-1,0,0,0],
+        [0,0,1,0],
+        [0,0,0,1]]) # 90 rotation, could need to try a -90 rotation based on sample
+    normal90 = utils.transform_pts(normal.reshape(1,3), rotate90_z).reshape(3,)
     
     # spacing of cuts
     cuts = np.linspace(posterior_cutoff, anterior_cutoff , num = 10)
     polygons, ys = [], []
     for cut in cuts:
         try:
-            path = mesh.section(plane_origin=[0,cut,0], plane_normal=[0,1,0])
-            slice,to_3d = path.to_planar()
+            # get trace along cut plane
+            path = mesh.section(plane_normal = normal90 ,plane_origin=[0,cut,0])
+            slice,to_3d = path.to_planar(normal = normal90)
         except:
-            break
+            break # end if cut is outside bone and produces no trace
 
         # get shapely object from path
         polygon = slice.polygons_closed[0]
-        # undo translation that is applied when moving from path3d to path2d
-        translation = to_3d[:2,3:].T[0] # find x and y translation that occured
-        polygon = shapely.affinity.translate(polygon, xoff=translation[0], yoff=translation[1])
+        pts = np.asarray(polygon.exterior.xy).T # extract points [nx3] matrix
+
+        # find seed point along plane
+        hc_pt_to2d = utils.transform_pts(hc_pt, utils.inv_transform(to_3d)) #project into plane space
+        seed_pt = hc_pt_to2d[:,:-1] #remove out of plane direction for now
+
+        # find circular portion of trace with rolling least squaress circle
+        rolling_cirle_fit()
 
         polygons.append(polygon)
         ys.append(cut)
-    df = pd.DataFrame({
+    df_y = pd.DataFrame({
     'poly':polygons,
     'y':ys
     })
