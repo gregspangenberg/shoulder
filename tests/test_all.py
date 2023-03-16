4c370318883c969ad157a6f6f4981fc05306b222
gregspangenberg <gspangen@westerneng.ca>
2023-03-16 15:39:51 -0400

    switch csys

--- tests/test_all.py
+++ tests/test_all.py
@@ -5,7 +5,7 @@ import pathlib
 print(pathlib.Path("./tests/test_bones").glob("*.stl"))
 for stl_bone in pathlib.Path("./tests/test_bones").glob("*.stl"):
     print(stl_bone.name)
-    h = base.CsysBone(str(stl_bone), csys="articular")
+    h = base.CsysBone(str(stl_bone), csys="transepi")
 
 
     h.line_plot().show()
