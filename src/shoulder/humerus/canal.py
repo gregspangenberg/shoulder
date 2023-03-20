531e5abb26c00bb66e61f564557245eaf99e8154
gregspangenberg <gspangen@westerneng.ca>
2023-03-19 22:02:57 -0400

    class rework of canal

--- src/shoulder/humerus/canal.py
+++ src/shoulder/humerus/canal.py
@@ -6,75 +6,114 @@ import pandas as pd
 import plotly.express as px
 import circle_fit
 from skspatial.objects import Line, Points
+from functools import cached_property
 
 
-"""this approach grabs the centroid from slices
-"""
+
+class Canal:
+    def __init__(self, mesh, cutoff_pcts, total_centroids):
+        self.cutoff_pcts = cutoff_pcts
+        self.total_centroids = total_centroids
+        self.mesh = mesh
+        self.mesh_oriented = self.mesh.copy()
+        # orient the above with a bounding box
+        self._transform_obb = self.mesh_oriented.apply_obb() 
+        
+    @cached_property
+    def transform_orient(self):
+        """rotates the humerus so the humeral head faces up (+ y-axis)
+
+        Args:
+            mesh (trimesh.mesh: mesh to rotate up
+
+        Returns:
+            mesh: rotated mesh
+            flip_y_T: transform to flip axis if it was performed
+            ct_T: transform back to CT space
+        """
+
+        """ The center of volume is now at (0,0,0) with the y axis of the CSYS being the long axis of the humerus.
+        The z being left-right and the x being up-down when viewed along the y-axis (humeral-axis)
+        Whether the humeral head lies in +y space or -y space is unkown. The approach to discover which end is which
+        is to take a slice on each end and see which shape is more circular. The more circular end is obviously the
+        humeral head.
+        """
+        # Get z bounds of box
+        y_limits = (self.mesh_oriented.bounds[0][-1], self.mesh_oriented.bounds[1][-1])
+
+        # look at slice shape on each end
+        humeral_end = (
+            0,
+            np.inf,
+        )  # (y_coordinate, residual_of_circle_fit), there is perhaps a better way of recording data
+        for y_limit in y_limits:
+            # make the slice
+            y_slice = 0.95 * y_limit  # move 5% inwards on the half, so 2.5% of total length
+            slice = self.mesh_oriented.section(plane_origin=[0, 0, y_slice], plane_normal=[0, 0, 1])
+            # returns the 2d view at plane and the transformation back to 3d space for each point
+            slice, to_3d =  slice.to_planar()  
+
+            # pull out the points along the shapes edge
+            slice_pts = np.array(slice.vertices)
+            xc, yc, r, residu = circle_fit.least_squares_circle(slice_pts)
+
+            # 1st pass, less than inf record, 2nd pass if less than 1st
+            if residu < humeral_end[1]: 
+                humeral_end = (y_limit, residu)
+
+        # if the y-coordinate of the humeral head is in negative space then
+        # we are looking to see if a flip was performed and if it was needed
+        # humeral_end is a set containing (y-coordinate, residual from circle fit)
+        if humeral_end[0] < 0:  
+            print('flipped')
+            # flip was reversed so update the ct_transform to refelct that
+            transform_flip_y = np.array([[-1, 0, 0, 0], [0, 1, 0, 0], [0, 0, -1, 0], [0, 0, 0, 1]])
+            self.mesh_oriented.apply_transform(transform_flip_y)
+        else:
+            print('not flipped')
+            transform_flip_y = np.array([[1, 0, 0, 0], [0, 1, 0, 0], [0, 0, 1, 0], [0, 0, 0, 1]])
+        
+        # add in flip that perhaps occured
+        transform = np.matmul(transform_flip_y, self._transform_obb)
+
+        return transform
 
 
-def orient_humerus(mesh):
-    """rotates the humerus so the humeral head faces up (+ y-axis)
+    @cached_property
+    def axis(self):
+        """calculates the centerline in region of humerus
 
-    Args:
-        mesh (trimesh.mesh: mesh to rotate up
+        Args:
+            mesh_file (str): path to mesh file
+            cutoff_pcts (list): cutoff for where centerline is to be fit between i.e [0.2,0.8] -> middle 60% of the bone
 
-    Returns:
-        mesh: rotated mesh
-        flip_y_T: transform to flip axis if it was performed
-        ct_T: transform back to CT space
-    """
-    # fit bounding box to mesh and orient view to along 'approximate' humeral canal axis
-    ct_T = mesh.apply_obb()  # applies transform to mesh and returns matrix used
+        Returns:
+            centerline: 2x3 matrix of xyz points at ends of centerline
+            cenerline_dir: 1x3 matrix of xyz direction of lline normal
+        """
+        
 
-    """ The center of volume is now at (0,0,0) with the y axis of the CSYS being the long axis of the humerus.
-    The z being left-right and the x being up-down when viewed along the y-axis (humeral-axis)
-    Whether the humeral head lies in +y space or -y space is unkown. The approach to discover which end is which
-    is to take a slice on each end and see which shape is more circular. The more circular end is obviously the
-    humeral head.
-    """
-    # Get z bounds of box
-    y_limits = (mesh.bounds[0][-1], mesh.bounds[1][-1])
+        # slice it !
+        centroids, cutoff_length = centroid_multislice(self.mesh_oriented, self.cutoff_pcts, self.total_centroids)
 
-    # look at slice shape on each end
-    humeral_end = (
-        0,
-        np.inf,
-    )  # (y_coordinate, residual_of_circle_fit), there is perhaps a better way of recording data
-    for y_limit in y_limits:
-        # make the slice
-        y_slice = 0.95 * y_limit  # move 5% inwards on the half, so 2.5% of total length
-        slice = mesh.section(plane_origin=[0, 0, y_slice], plane_normal=[0, 0, 1])
-        (
-            slice,
-            to_3d,
-        ) = (
-            slice.to_planar()
-        )  # returns the 2d view at plane and the transformation back to 3d space for each point
+        # transform back
+        centroids_ct = utils.transform_pts(centroids, utils.inv_transform(self.transform_orient))
 
-        # pull out the points along the shapes edge
-        slice_pts = np.array(slice.vertices)
-        xc, yc, r, residu = circle_fit.least_squares_circle(slice_pts)
+        # calculate centerline
+        canal_fit = Line.best_fit(Points(centroids_ct))
 
-        if (
-            residu < humeral_end[1]
-        ):  # 1st pass, less than inf record, 2nd pass if less than 1st
-            humeral_end = (y_limit, residu)
+        # repersent centerline as two points at the extents of the cutoff
+        canal_prox = canal_fit.point + (
+            canal_fit.direction * (cutoff_length / 2)
+        )  # canal_fit.point is in the middle
+        canal_dstl = canal_fit.point - (
+            canal_fit.direction * (cutoff_length / 2)
+        )
 
-    # if the y-coordinate of the humeral head is in negative space then
-    # we are looking to see if a flip was performed and if it was needed
-    if (
-        humeral_end[0] < 0
-    ):  # humeral_end is a set containing (y-coordinate, residual from circle fit)
-        # print('flipped')
-        # flip was reversed so update the ct_transform to refelct that
-        flip_y_T = np.array([[-1, 0, 0, 0], [0, 1, 0, 0], [0, 0, -1, 0], [0, 0, 0, 1]])
-        mesh.apply_transform(flip_y_T)
-    else:
-        # print('not flipped')
-        flip_y_T = np.array([[1, 0, 0, 0], [0, 1, 0, 0], [0, 0, 1, 0], [0, 0, 0, 1]])
+        canal_pts_ct = np.array([canal_prox, canal_dstl])
 
-    return mesh, ct_T, flip_y_T
-
+        return canal_pts_ct
+    
 
 def centroid_multislice(mesh, cutoff_pcts, num_centroids):
     """Slices bone along long axis between the specified cutoff points and returns
@@ -115,88 +154,3 @@ def centroid_multislice(mesh, cutoff_pcts, num_centroids):
     centroids = np.concatenate(centroids, axis=0)
 
     return centroids, cutoff_length
-
-
-def centerline_plot(mesh, centerline, num_centroids):
-    # sample mesh surface
-    dots = trimesh.sample.sample_surface_even(mesh, 500)
-    df_d = pd.DataFrame(dots[0])
-    df_d = df_d.rename({0: "x", 1: "y", 2: "z"}, axis=1)
-
-    # sample centerline
-    df_c = pd.DataFrame(centerline)
-    z_100 = np.linspace(
-        df_c.iloc[0, :],
-        df_c.iloc[
-            1:,
-        ],
-        num=num_centroids,
-    ).reshape(-1, 3)
-    df_c = pd.DataFrame(z_100.round(3))  # remove uneeded precision
-    df_c = df_c.rename({0: "x", 1: "y", 2: "z"}, axis=1)
-
-    # plot
-    join_dfs = {"bone": df_d, "axis": df_c}
-    df = pd.concat([df.assign(identity=k) for k, df in join_dfs.items()])
-
-    fig = px.scatter_3d(df, x="x", y="y", z="z", color="identity")
-    fig.update_layout(
-        scene_aspectmode="data"
-    )  # plotly defualts into focing 3d plots to be distorted into cubes, this prevents that
-
-    return fig
-
-
-def axis(mesh, cutoff_pcts, num_centroids):
-    """calculates the centerline in region of humerus
-
-    Args:
-        mesh_file (str): path to mesh file
-        cutoff_pcts (list): cutoff for where centerline is to be fit between i.e [0.2,0.8] -> middle 60% of the bone
-
-    Returns:
-        centerline: 2x3 matrix of xyz points at ends of centerline
-        cenerline_dir: 1x3 matrix of xyz direction of lline normal
-    """
-    alt_mesh = mesh.copy()  # alt_mesh means altered mesh
-
-    # rotate so humerus is up
-    alt_mesh, to_ct_transform, flip_transform = orient_humerus(alt_mesh)
-
-    # slice it !
-    centroids, cutoff_length = centroid_multislice(alt_mesh, cutoff_pcts, num_centroids)
-
-    # add in flip that perhaps occured
-    to_ct_transform = np.matmul(flip_transform, to_ct_transform)
-
-    # transform back
-    centroids_ct = utils.transform_pts(centroids, flip_transform)
-    centroids_ct = utils.transform_pts(centroids, utils.inv_transform(to_ct_transform))
-
-    # calculate centerline
-    points = Points(centroids_ct)
-    centerline_fit = Line.best_fit(points)
-
-    # repersent centerline as two points at the extents of the cutoff
-    centerline1 = centerline_fit.point + (
-        centerline_fit.direction * (cutoff_length / 2)
-    )  # centerline_fit.point is in the middle
-    centerline2 = centerline_fit.point - (
-        centerline_fit.direction * (cutoff_length / 2)
-    )
-
-    # calculate the transform needed to go to new CSYS from CT CSYS
-    # transform = utils.rot_matrix_3d(np.array(centerline_fit.direction), [0, 0, 1])
-    transform = trimesh.geometry.align_vectors(np.array(centerline_fit.direction), [0, 0, 1])[:3,:3] #remove row and column
-    # calculate rotation matrix so z+
-    pt = mesh.centroid.reshape(3, 1)  # new CSYS has centroid at [0,0,0]
-    transform = np.c_[
-        transform, -1 * np.matmul(transform, pt)
-    ]  # add in translation to centroid
-    transform = np.r_[
-        transform, np.array([[0, 0, 0, 1]])
-    ]  # no scaling occurs so leave as default
-
-    centerline_pts_ct = np.array([centerline1, centerline2])
-
-    return centerline_pts_ct, transform
