06cc51630870396c5460a66c7a6db57e28c68c62
gregspangenberg <gspangen@westerneng.ca>
2023-05-04 01:24:30 -0400

    refactor

---
+++ tests/test1.py
@@ -0,0 +1,9 @@
+import shoulder
+import pathlib
+import numpy
+
+print(pathlib.Path("./tests/test_bones").glob("*.stl"))
+for stl_bone in pathlib.Path("./tests/test_bones").glob("*.stl"):
+    print(stl_bone.name)
+    shoulder.
+    break
