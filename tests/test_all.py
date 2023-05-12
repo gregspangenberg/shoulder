15afd6fd8b3692da4ce3624f6744bb833c2fbd18
gregspangenberg <gspangen@westerneng.ca>
2023-05-12 15:17:46 -0400

    remove  unneeded canal references

--- tests/test_all.py
+++ tests/test_all.py
@@ -6,13 +6,14 @@ for stl_bone in pathlib.Path("./tests/test_bones").glob("*.stl"):
     print(stl_bone.name)
 
     h = shoulder.Humerus(stl_bone)
-    
+
     # h.canal.axis([0.5, 0.8])
     h.canal.axis()
     h.trans_epiconylar.axis()
     h.anatomic_neck.plane()
     h.bicipital_groove.axis()
-    h.apply_csys_canal_transepiconylar()
+    h.anatomic_neck.
+    # h.apply_csys_canal_transepiconylar()
 
     p = shoulder.Plot(h)
 
