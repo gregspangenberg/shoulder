0d571ef7bef0640e32056bdbd2a4d80e6d79ecef
gregspangenberg <gspangen@westerneng.ca>
2023-09-12 17:34:42 -0400

    undo

--- tests/validate_arthritic.py
+++ tests/validate_arthritic.py
@@ -2,7 +2,9 @@ import shoulder
 import pathlib
 import numpy as np
 
-stls = pathlib.Path("../shoulder_data/bones/arthritic").glob("*.stl")
+stls = pathlib.Path("/home/gspangen/projects/shoulder_data/bones/arthritic").glob(
+    "*.stl"
+)
 for i, stl_bone in enumerate(stls):
     print(stl_bone.name)
 
@@ -12,4 +14,6 @@ for i, stl_bone in enumerate(stls):
     h.apply_csys_canal_articular(np.random.rand(2, 3))
 
     p = shoulder.Plot(h, opacity=1.0)
-    p.figure.write_html("../shoulder_data/viz/" + stl_bone.stem + ".html")
+    p.figure.write_html(
+        "/home/gspangen/projects/shoulder_data/viz/" + stl_bone.stem + ".html"
+    )
--- tests/validate_health.py
+++ tests/validate_health.py
@@ -1,20 +1,24 @@
 import shoulder
 import pathlib
 
-stls = pathlib.Path("../shoulder_data/bones/non_arthritic").glob("*.stl")
+stls = pathlib.Path("/home/gspangen/projects/shoulder_data/bones/non_arthritic").glob(
+    "*.stl"
+)
 for i, stl_bone in enumerate(stls):
     print(stl_bone.name)
     h = shoulder.Humerus(stl_bone)
 
     h.canal.axis([0.5, 0.8])
     h.trans_epiconylar.axis()
-    try:
-        h.anatomic_neck.plane()
-    except:
-        print("whoops")
 
     h.bicipital_groove.axis(cutoff_pcts=[0.3, 0.75], deg_window=7)
     h.apply_csys_canal_transepiconylar()
 
     p = shoulder.Plot(h, opacity=1.0)
-    p.figure.write_html("../shoulder_data/viz/" + stl_bone.stem + ".html")
+    p.figure.write_html(
+        "/home/gspangen/projects/shoulder_data/viz" + stl_bone.stem + ".html"
+    )
+
+    h.bicipital_groove._X.to_csv("X.csv", index=False)
+    print("ye")
+    break
