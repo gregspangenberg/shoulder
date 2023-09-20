a1e92ee8ccd7ae4896c2b8f86ee01e881e5e5b60
gregspangenberg <gspangen@westerneng.ca>
2023-09-20 16:49:51 -0400

    cleanup test statements

--- tests/validate_health.py
+++ tests/validate_health.py
@@ -8,16 +8,13 @@ for i, stl_bone in enumerate(stls):
     print(stl_bone.name)
     h = shoulder.Humerus(stl_bone)
 
-    h.canal.axis()
-    # print(h.canal._axis)
-    # h.trans_epiconylar.axis()
+    h.canal.axis([0.5, 0.8])
+    h.trans_epiconylar.axis()
 
-    # h.bicipital_groove.axis(cutoff_pcts=[0.3, 0.75], deg_window=7)
+    h.bicipital_groove.axis(cutoff_pcts=[0.3, 0.75], deg_window=7)
     h.apply_csys_canal_transepiconylar()
-    # h.apply_csys_obb()
 
-    p = shoulder.Plot(h, opacity=0.8)
+    p = shoulder.Plot(h, opacity=1.0)
     p.figure.write_html(
-        "/home/gspangen/projects/shoulder_data/viz/" + stl_bone.stem + ".html"
+        "/home/gspangen/projects/shoulder_data/viz" + stl_bone.stem + ".html"
     )
-    break
