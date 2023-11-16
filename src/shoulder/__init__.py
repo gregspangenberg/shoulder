9ee037948032b0dbac9c45e11d6d2ffeda0355a6
gregspangenberg <gspangen@westerneng.ca>
2023-11-16 12:02:15 -0500

    incr version

--- src/shoulder/__init__.py
+++ src/shoulder/__init__.py
@@ -2,4 +2,4 @@ from shoulder.bone import Humerus, ProximalHumerus
 from shoulder.plotting import Plot
 from shoulder.arthroplasty import HumeralHeadOsteotomy, HumeralImplantation
 
-__version__ = "1.2.7"
+__version__ = "1.2.8"
